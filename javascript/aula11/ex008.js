8da615ebe1c1362ab1a56d22f0ecb49f51217151
Mendex002 <joaovictor.mendez2000@gmail.com>
2025-12-18 19:32:37 -0300

    ex008 de condição simples

--- javascript/aula11/ex008.js
+++ javascript/aula11/ex008.js
@@ -1,3 +1,7 @@
-var vel = 60.5
+var vel = 59
 
 console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60 ) { // condição simples!
+    console.log(`você ultrapassou a velocidade permitida. MUltado!`)
+}
+console.log (`dirija sempre usando cinto de segurança `)
